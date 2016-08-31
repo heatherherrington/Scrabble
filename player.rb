8381b451a3c28513c94eff3fcb9fe2095a073135
heatherherrington <redherring@gmail.com>
2016-08-31 11:27:19 -0700

    WIP - satisfied name, plays, total_score, and won?, need score of word in play

--- player.rb
+++ player.rb
@@ -1,3 +1,5 @@
+require_relative "scoring"
+
 module Scrabble
   class Player
     attr_reader :name, :words_played
@@ -17,7 +19,22 @@ module Scrabble
 
     def play(word)
       @words_played << word
+      if won?
+        return false
+      end
       return word
+    end
+
+    def total_score
+      player_total_score = 0
+      @words_played.each do |word|
+        player_total_score += Scrabble::Scoring.score(word)
+      end
+      return player_total_score
+    end
+
+    def won?
+      total_score > 100
     end
   end
 end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -44,5 +44,38 @@ describe Scrabble::Player do
     it "the last element of the array should equal the input value" do
        player.play("fuzzy").must_equal(player.words_played.last)
     end
+
+    it "should return false if player has won" do
+      player.play("zzzzzzz").must_equal(false)
+    end
+  end
+
+  describe "#total score" do
+    let(:player) { Scrabble::Player.new("Foot Foot") }
+
+    it "should return a Fixnum" do
+      player.total_score.must_be_kind_of(Fixnum)
+    end
+
+    it "should be total score of words played array" do
+      player.play("cat")
+      player.play("dog")
+      player.play("bee")
+      player.total_score.must_equal(15)
+    end
+  end
+
+  describe "#won?" do
+    let(:player) { Scrabble::Player.new("Foot Foot") }
+
+    it "should return a true" do
+      player.play("zzzzzzz")
+      player.won?.must_equal(true)
+    end
+
+    it "should return a false" do
+      player.play("bah")
+      player.won?.must_equal(false)
+    end
   end
 end
