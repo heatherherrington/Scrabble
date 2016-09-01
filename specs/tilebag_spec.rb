aa687b99a314ace63dcbe653c0010e0e8559749b
Allison Hoke <allisonhoke@gmail.com>
2016-09-01 12:31:15 -0700

    WIP

--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -3,11 +3,19 @@ require_relative '../tilebag'
 
 describe Scrabble::Tilebag do
   describe "#initialize" do
+    let(:tilebag) { Scrabble::Tilebag.new }
 
     it "can create a new instance of tile bag" do
-      tilebag.must_be_instance_of(Scrabble::TileBag)
+      tilebag.must_be_instance_of(Scrabble::Tilebag)
     end
+  end
 
+  describe "#draw_tiles(num)" do
+    let(:tilebag) { Scrabble::Tilebag.new }
+
+    it "should raise an ArgumentError if input is not a Fixnum" do
+      proc { tilebag.draw_tiles("") }.must_raise(ArgumentError)
+    end
   end
 
 end
--- tilebag.rb
+++ tilebag.rb
@@ -1,38 +1,52 @@
 module Scrabble
   class Tilebag
+    attr_reader :tilebag
 
     NUMBER_OF_LETTERS = {
-      A: 9,
-      B: 2,
-      C: 2,
-      D: 4,
-      E: 12,
-      F: 2,
-      G: 3,
-      H: 2,
-      I: 9,
-      J: 1,
-      K: 1,
-      L: 4,
-      M: 2,
-      N: 6,
-      O: 8,
-      P: 2,
-      Q: 1,
-      R: 6,
-      S: 4,
-      T: 6,
-      U: 4,
-      V: 2,
-      W: 2,
-      X: 1,
-      Y: 2,
-      Z: 1
+      "A" => 9,
+      "B" => 2,
+      "C" => 2,
+      "D" => 4,
+      "E" => 12,
+      "F" => 2,
+      "G" => 3,
+      "H" => 2,
+      "I" => 9,
+      "J" => 1,
+      "K" => 1,
+      "L" => 4,
+      "M" => 2,
+      "N" => 6,
+      "O" => 8,
+      "P" => 2,
+      "Q" => 1,
+      "R" => 6,
+      "S" => 4,
+      "T" => 6,
+      "U" => 4,
+      "V" => 2,
+      "W" => 2,
+      "X" => 1,
+      "Y" => 2,
+      "Z" => 1
     }
 
+      def initialize
+        @tilebag = []
 
+        NUMBER_OF_LETTERS.each do |key, value|
+          value.times do
+            @tilebag << key
+          end
+        end
+      end
 
+      def draw_tiles(num)
+        if num.class != Fixnum
+          raise ArgumentError.new("Please enter a number")
+        end 
 
+      end
 
   end
 end
